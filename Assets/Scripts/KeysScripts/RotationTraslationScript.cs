7dbfdbdd05928cf1b21db7d9aed7d0f3aae1424b
airesteban <airascaesteban@gmail.com>
2022-03-01 13:44:10 -0300

    Gel generators have now better speed, key now rotates

--- Assets/Scripts/KeysScripts/RotationTraslationScript.cs
+++ Assets/Scripts/KeysScripts/RotationTraslationScript.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class RotationTraslationScript : MonoBehaviour
 {
     [SerializeField] GameObject keyPrefab;
+    [SerializeField] float angleRotation = 1f;
+    [SerializeField] Transform[] wayPoints;
+    private int actIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        actIndex = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RotateAndTranslate();
+        RotateObject();
     }
 
-    private void RotateAndTranslate()
+    private void RotateObject()
     {
-        keyPrefab.transform.position.y += 10f;
+        keyPrefab.transform.Rotate(new Vector3(1f, 0f, 0f), angleRotation);
     }
 }
